284bb51a8a5a8e883962849c54dbf967075d70f7
Ignacio Alonso <ignacioalonso856@gmail.com>
2023-03-21 18:17:19 -0300

    primer commit

---
+++ src/guia_2_ejercicio_2/Guia_2_Ejercicio_2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia_2_ejercicio_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nacho
+ */
+public class Guia_2_Ejercicio_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer=new Scanner(System.in);
+        String nombre;
+        nombre=leer.next();
+        System.out.println(nombre);
+    }
+    
+}
